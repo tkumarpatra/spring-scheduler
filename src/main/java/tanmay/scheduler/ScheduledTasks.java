b4681d0acc16a17aa1caeb2e9c3bf90184b01c6e
t@nm@y <writetotkpatra@gmail.com>
2019-02-04 01:39:38 +0530

    initial commit

---
+++ src/main/java/tanmay/scheduler/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package tanmay.scheduler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("Logging from the scheduler {}", dateFormat.format(new Date()));
+    }
+}
